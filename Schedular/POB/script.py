10b0d6f72a4894ef2be999b5d37fa78b61032e01
Gursimran Singh <guri21895@gmail.com>
2020-12-08 17:56:48 -0500

    POB_Schedular

---
+++ Schedular/POB/script.py
@@ -0,0 +1,19 @@
+file = open('small.txt', 'r') 
+op = file.readline()
+total_nodes = 20
+op_num = 1
+while(op):
+	print(op)
+	data = op.split()
+	host, port = data[1], int(data[2])
+	if(data[0] == "send"):
+		operation = Operation(op_num, data[3], int(data[4]), int(data[5]))
+		node.broadcast(host, port, operation, type = "send")
+		while(Counter.d.get(op_num,0) < total_nodes): continue
+	else:
+		operation = Operation(op_num)
+		node.broadcast(host, port, operation, type = "mine")
+		while(Counter.d.get(op_num,0) < total_nodes*2): continue
+	print("Stage " + str(op_num) + " Completed")
+	op_num += 1
+	op = file.readline()
